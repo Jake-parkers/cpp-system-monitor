795679d04dfd7c69b959eaf114ba137a092a4fdf
Jake-parkers <kwakujosh@gmail.com>
2022-01-23 14:48:29 +0100

    cleanup

--- src/format.cpp
+++ src/format.cpp
@@ -10,15 +10,23 @@ using std::string;
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) {
-  int hours, minutes;
+  int hours, minutes, secs;
 
+  seconds = seconds % (24 * 3600);
+  hours = seconds / 3600;
+
+  seconds %= 3600;
   minutes = seconds / 60;
-  seconds = seconds % 60; // remaining secs after removing minutes
-  hours = minutes / 60;
-  minutes = minutes % 60; // remaining minutes after removing hours
+
+  seconds %= 60;
+  secs = seconds;
+//  minutes = seconds / 60;
+//  seconds = seconds % 60; // remaining secs after removing minutes
+//  hours = minutes / 60;
+//  minutes = minutes % 60; // remaining minutes after removing hours
   string minute_format = (minutes < 10) ? ":0" : ":";
   string second_format = (seconds < 10) ? ":0" : ":";
   string hour_format = (hours < 10) ? "0" : "";
 
-  return hour_format + std::to_string(hours) + minute_format + std::to_string(minutes) + second_format + std::to_string(seconds);
+  return hour_format + std::to_string(hours) + minute_format + std::to_string(minutes) + second_format + std::to_string(secs);
 }
--- src/process.cpp
+++ src/process.cpp
@@ -71,5 +71,5 @@ long int Process::UpTime() {
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
 bool Process::operator<(Process const& a) const {
-  return a.m_cpu_usage < m_cpu_usage;
+  return m_ram < a.m_ram;
 }
